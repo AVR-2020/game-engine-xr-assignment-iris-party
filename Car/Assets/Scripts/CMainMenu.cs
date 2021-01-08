b9c2c4f67b9b238317428b79c7f888c78d767a4c
Chris Angga <chrisnady.anggaiswara16@mhs.if.its.ac.id>
2021-01-08 23:19:51 +0700

    simple startgame
    still not polished

---
+++ Car/Assets/Scripts/CMainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMainMenu : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject HowtoPlay;
+
+    
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayNowButton()
+    {
+        // move to next scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("car");
+    }
+
+    public void HowtoPLayButton()
+    {
+        
+        MainMenu.SetActive(false);
+        HowtoPlay.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        
+        MainMenu.SetActive(true);
+        HowtoPlay.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        
+        Application.Quit();
+    }
+}
